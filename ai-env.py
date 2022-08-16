49b01819c08a43dcc1937c6c01c79f9475de7bc3
Gongsta <gong.steven@hotmail.com>
2022-08-16 15:12:49 -0400

    add daily changes

--- ai-env.py
+++ ai-env.py
@@ -25,6 +25,7 @@ class PokerEnv(gym.Env):
 		self.observation_space = spaces.Dict(
 			{
 				"pot_balance": spaces.Discrete(50), # Pot bet between 0...50 BB
+				"dealer_position": spaces.Discrete(2), # 0 or 1 depending on whether we are the dealer, this is important for aggression
 				# "player_bet": spaces.Discrete(50), #represents how much the player has already put in the pot
 				"opponent_bet": spaces.Discrete(50), # This is what the opponent bets, so you need to match it, so that would be your reward
 				"combined_hand": spaces.MultiBinary(52) # Array of 52 bits where each bit will be either 0 or 1
@@ -38,7 +39,7 @@ class PokerEnv(gym.Env):
 	def _get_obs(self):
 		return {
 			"pot_balance": self.env.total_pot_balance,
-			# TODO:"opponent_bet": self.env.
+			"opponent_bet": self.env.stage_pot_balance, # TODO: Will need to be changed for raises and stuff. Since this is just one turn, and no raises 
 			"combined_hand": self.hand # Array
 
 		}
@@ -48,57 +49,26 @@ class PokerEnv(gym.Env):
 			"opponent_hand": self.env.players[1].hand
 		}
 
-
-	def _handle_game_stage(self, action=""): # A modification of the PokerEnvironment handle_game_stage() function
-		if self.finished_playing_game_stage:
-			if self.game_stage != 1:
-				self.update_player_balances_at_end_of_stage()
-				self.move_stage_to_total_pot_balance()
-			self.game_stage += 1
-			
-			if self.game_stage == 2:
-				self.play_preflop()
-			elif self.game_stage == 3:
-				self.play_flop()
-			elif self.game_stage == 4:
-				self.play_turn()
-			elif self.game_stage == 5:
-				self.play_river()
-			else:
-				if self.game_stage == 6: # We reached the river, and are now in the showdown. We need the evaluator to get the winners, set all losers to playing_current_round false
-					evaluator = Evaluator()
-					
-					indices_of_potential_winners = []
-					for idx, player in enumerate(self.players):
-						if player.playing_current_round: 
-							indices_of_potential_winners.append(idx)
-							hand = CombinedHand(self.community_cards + player.hand)
-							evaluator.add_hands(hand)
-					
-					winners = evaluator.get_winner()
-					for player in self.players:
-						player.playing_current_round = False
-
-					for winner in winners:
-						self.players[indices_of_potential_winners[winner]].playing_current_round = True
-
-				self.distribute_pot_to_winning_players()
-				self.game_stage = 1
-				self.finished_playing_game_stage = False # on the next call of the handler, we will start a new round
-		else:
-			if self.game_stage == 1:
-				self.start_new_round()
-			else:
-				self.play_current_stage(action)
-
 	def step(self, action):
+		"""
+		There are two cases:
+		1. We are the dealer, in which case the opponent plays first
+		2. The opponent is the dealer, in which ase we play first
+		
+		The idea is this: We play, and then get an observation on how the opponent reacts based on how we play.
+		However, if we go second, our observation will be based on the fact that there is nothing put on the pot, and action is on us.
+		"""
 		# action == 0 -> Fold
 		# action == 1 -> Check
 
 		self._update_hand()
-		self._handle_game_stage()
+		if self.env.dealer_button_position == 0: # Action is on the opponent first
+			self.env.handle_game_stage()
+		
+		else: # Action is on us first
         # done = np.array_equal(self._agent_location, self._target_location)
-        # reward = 1 if done else 0  # Binary sparse rewards
+		if self.env.game_stage == 1:
+        reward = 
         observation = self._get_obs()
         info = self._get_info()
 
@@ -107,11 +77,9 @@ class PokerEnv(gym.Env):
 
         return observation, reward, done, info
 		
-		
-		return observation, reward, done, info
 	def reset(self):
-		...
 		return observation  # reward, done, info can't be included
+
 	def render(self, mode='human'):
 		...
 	def close (self):
